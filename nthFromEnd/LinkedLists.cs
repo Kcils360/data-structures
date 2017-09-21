72905366aeb680d049002d88d94088d0a234ae20
Gregory Dukes <kcils360@gmail.com>
2017-09-20 17:02:15 -0700

    finished code challenge find nth from end

---
+++ nthFromEnd/LinkedLists.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIndMiddle
+{
+    public class LinkedList
+    {
+        private Node head;
+        private Node current;
+        public int Count;
+
+        public LinkedList()
+        {
+            head = new Node();
+            current = head;
+        }
+
+        public void AddAtLast(object data)
+        {
+            Node newNode = new Node();
+            newNode.Value = data;
+            current.Next = newNode;
+            current = newNode;
+            Count++;
+        }
+
+        public void PrintAllNodes()
+        {
+            Console.Write("Head --"); //head sentinel
+            Node curr = head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Value);
+                Console.Write("--");
+            }
+            Console.Write("null"); //tail sentinel
+        }
+
+        public void FindMiddle()
+        {
+            Node slow = head;
+            Node fast = head;
+            while (fast != null && fast.Next != null)
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+            }
+            Console.WriteLine(slow.Value);
+        }
+
+        public void NthFromEnd(int n)
+        {
+            Node second = head;
+            Node current = head;
+
+            for(int i = 0; i < n-1; i++)
+            {
+                if (current != null)
+                {
+                    current = current.Next;
+                }
+                else
+                {
+                    Console.WriteLine("null");   //if n is bigger than list length
+                }
+            }
+            while(current.Next != null)
+            {
+                current = current.Next;
+                second = second.Next;
+            }
+            Console.WriteLine(second.Value);
+        }
+
+    }
+}
