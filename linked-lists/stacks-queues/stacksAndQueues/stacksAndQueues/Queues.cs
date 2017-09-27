480abb9422dcbf61a9421578efdf821929177c7d
Gregory Dukes <kcils360@gmail.com>
2017-09-26 21:19:51 -0700

    it's built anyway

---
+++ linked-lists/stacks-queues/stacksAndQueues/stacksAndQueues/Queues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stacksAndQueues
+{
+    class Queues
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public Queue(int Data)
+        {
+            Head = new Node(Data);
+            Tail = Head;
+        }
+        public void Enqueue(int data)
+        {
+            Tail.Next = new Node(data);
+            Tail = Tail.Next;
+        }
+
+        public int Dequeue()
+        {
+            if(Head == Tail)
+            {
+                Node temp = Head.Next;
+                Head.Next = null;
+                int Value = Head.Data;
+                Head = temp;
+                return Value;
+            }
+        }
+    }
+}
---
+++ linked-lists/stacks-queues/stacksAndQueues/stacksAndQueues/Stacks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stacksAndQueues
+{
+    class Stacks
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public Stack(int Data)
+        {
+            Head = new Node(Data);
+        }
+        public void Push(int Data)
+        {
+            Node temp = Head;
+            Head = new Node(Data);
+            Head.Next = temp;
+        }
+        public int Pop()
+        {
+            if (Head == Tail)
+            {
+                Tail = null;
+            }
+            Node temp = Head.Next;
+            int value = Head.Data;
+            Head = temp;
+            return value;
+        }
+        
+    }
+}
